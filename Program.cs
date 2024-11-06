f3c7d6da51acb8d6b16937459a5f11488de77758
Robert Mack <robertmack123123@gmail.com>
2024-11-06 09:36:29 -0600

    Setup commit

--- Program.cs
+++ Program.cs
@@ -1,3 +1,85 @@
+
+
+List<Dog> dogs = new List<Dog>
+{
+    new Dog()
+    {
+        Id = 1,
+        Name = "Buddy",
+        WalkerId = 1,
+        CityId = 1
+    },
+    new Dog()
+    {
+        Id = 2,
+        Name = "Max",
+        WalkerId = 2,
+        CityId = 2
+    },
+    new Dog()
+    {
+        Id = 3,
+        Name = "Bella",
+        WalkerId = null,  // No walker assigned yet
+        CityId = 1
+    },
+    new Dog()
+    {
+        Id = 4,
+        Name = "Charlie",
+        WalkerId = 3,
+        CityId = 3
+    }
+};
+
+List<Walker> walkers = new List<Walker>
+{
+    new Walker()
+    {
+        Id = 1,
+        Name = "Sarah"
+    },
+    new Walker()
+    {
+        Id = 2,
+        Name = "Jake"
+    },
+    new Walker()
+    {
+        Id = 3,
+        Name = "Lily"
+    },
+    new Walker()
+    {
+        Id = 4,
+        Name = "Tom"
+    }
+};
+
+List<City> cities = new List<City>
+{
+    new City()
+    {
+        Id = 1,
+        Name = "Springfield"
+    },
+    new City()
+    {
+        Id = 2,
+        Name = "Riverdale"
+    },
+    new City()
+    {
+        Id = 3,
+        Name = "Sunnyvale"
+    },
+    new City()
+    {
+        Id = 4,
+        Name = "Hill Valley"
+    }
+};
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
